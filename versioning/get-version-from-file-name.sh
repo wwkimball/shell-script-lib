cc814bbbb422d4843e971203fbeff1280a9a1deb
William Kimball <30981667+wwkimball@users.noreply.github.com>
2025-04-24 10:55:38 -0500

    Expand version number handling

---
+++ versioning/get-version-from-file-name.sh
@@ -0,0 +1,35 @@
+################################################################################
+# Defines a function, getVersionFromFileName, which provides logic that can
+# identify the version of a product from its file-name, provided the version
+# number appears in the file-name in a reliable, identifiable way.
+#
+# Copyright 2001, 2018 William W. Kimball, Jr. MBA MSIS
+################################################################################
+# Libraries must not be directly executed
+if [ -z "${BASH_SOURCE[1]}" ]; then
+	echo "ERROR:  Attempt to directly execute $0." >&2
+	exit 1
+fi
+
+function getVersionFromFileName {
+	local fileSpec fileName versionNumber
+	fileSpec=${1:?"ERROR:  A file-name must be provided as the first positional argument to ${FUNCNAME[0]}."}
+	fileName=${fileSpec##*/}
+
+	# Strip off any RPM-style Release Tag
+	if [[ $fileName =~ ^(.*[[:digit:]]+(\.[[:digit:]]+)*)-[[:digit:]].*$ ]]; then
+		fileName=${BASH_REMATCH[1]}
+	fi
+
+	# Attempt to identify the version number
+	versionNumber=
+	if [[ $fileName =~ ^.+-([[:digit:]]+(\.[[:digit:]]+)*).*$ ]]; then
+		versionNumber=${BASH_REMATCH[1]}
+	elif [[ $fileName =~ ^.+[^[:digit:]\.]([[:digit:]]+(\.[[:digit:]]+)*).*$ ]]; then
+		versionNumber=${BASH_REMATCH[1]}
+	else
+		errorOut 70 "A version number could not be found in file-name, ${fileName}."
+	fi
+
+	echo "$versionNumber"
+}

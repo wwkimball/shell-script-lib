1711d4385674c8859b87918fab5b75b90c05c837
William Kimball <30981667+wwkimball@users.noreply.github.com>
2025-08-14 17:09:52 -0500

    Add getDockerEnvironmentVariable

---
+++ docker/functions/get-docker-environment-variable.sh
@@ -0,0 +1,117 @@
+################################################################################
+# Implement the getDockerEnvironmentVariable function.
+################################################################################
+# Libraries must not be directly executed
+if [ -z "${BASH_SOURCE[1]}" ]; then
+	echo "ERROR:  Attempt to directly execute $0." >&2
+	exit 1
+fi
+
+# Dynamically load the common logging functions
+if [ -z "$LIB_DIRECTORY" ]; then
+	# The common library directory is not set, so set it to the default value
+	MY_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+	PROJECT_DIRECTORY="$(cd "${MY_DIRECTORY}/../../../" && pwd)"
+	LIB_DIRECTORY="${PROJECT_DIRECTORY}/lib"
+	readonly MY_DIRECTORY PROJECT_DIRECTORY LIB_DIRECTORY
+fi
+setLoggerSource="${LIB_DIRECTORY}/logging/set-logger.sh"
+if ! source "$setLoggerSource"; then
+	echo "ERROR:  Unable to source ${setLoggerSource}!" >&2
+	exit 2
+fi
+unset setLoggerSource
+
+###
+# Get the value of a named environment variable from any possible Docker source.
+#
+# This function searches for the specified environment variable in the following
+# locations, in preferred order:
+# 1. Environment variables of the current shell
+# 2. Any available Docker Compose YAML files in the specified directory,
+#    filtered by the deployment stage (when provided)
+# 3. .env* files in the specified directory, filtered by the deployment stage
+#    (when provided)
+#
+# Arguments:
+# @param string $1 Name of the environment variable to resolve; this value is
+#                  case-sensitive.
+# @param string $2 Directory containing Docker Compose YAML and/or .env files.
+# @param string $3 Name of the deployment stage.  Used to disambiguate between
+#                  files like docker-compose.staging.yaml versus
+#                  docker-compose.production.yaml, .env.staging versus
+#                  .env.production, and so on.  Note that docker-compose.yaml
+#                  and .env are always read when either exists.
+#
+# @return integer One of:
+#   0 on success
+#   1 when any required argument are empty or null
+#   2 when no Docker Compose YAML or .env* files could be found
+#   3 when the environment variable could not be resolved
+# STDOUT:  The resolved value of the environment variable on success
+# STDERR:  An error message on failure
+#
+# @example With a Docker Compose file
+#   getDockerEnvironmentVariable "MY_ENV_VAR" "docker/docker-compose.yaml"
+#
+# @example Without a Docker Compose file
+#   getDockerEnvironmentVariable "MY_ENV_VAR"
+##
+function getDockerEnvironmentVariable {
+	local varName=${1:?"ERROR:  A Docker environment variable name must be provided as the first positional argument to ${FUNCNAME[0]}"}
+	local dockerDir=${2:?"ERROR:  The Docker files directory must be provided as the second positional argument to ${FUNCNAME[0]}"}
+	local deploymentStage=${3:?"ERROR:  The deployment stage must be provided as the third positional argument to ${FUNCNAME[0]}"}
+	local tempBakedFile possibleValue returnState=0
+
+	# First, check the shell environment
+	if [ -n "${!varName}" ]; then
+		echo "${!varName}"
+		return 0
+	}
+
+	# Next, check the Docker Compose YAML file(s)
+	tempBakedFile=$(mktemp -t "docker-compose-XXXXXX.yaml")
+	if dynamicBakeComposeFile "$tempBakedFile" "$deploymentStage" "$dockerDir"
+	then
+		# Check for the variable within the baked Docker Compose file
+		possibleValue=$(yaml-get --query="(/services/**/environment/${varName})[0]" "$tempBakedFile" 2>/dev/null)
+		if [ 0 -eq $? ] && [ -n "$possibleValue" ]; then
+			echo "$possibleValue"
+			rm -f "$tempBakedFile"
+			return 0
+		fi
+	else
+		returnState=$?
+		if [ "$returnState" -eq 2 ]; then
+			logError "No Docker Compose YAML files found in ${dockerDir}."
+		else
+			logError "Failed to bake the Docker Compose file:  $tempBakedFile"
+		fi
+		rm -f "$tempBakedFile"
+		return 2
+	fi
+
+	# Finally, check for the value in any available .env files
+	tempBakedFile=$(mktemp -t "docker-env-XXXXXX")
+	if dynamicMergeEnvFiles "$tempBakedFile" "$dockerDir" "$deploymentStage"
+	then
+		# Check for the variable within the merged .env file
+		possibleValue=$(grep -E "^${varName}=" "$tempBakedFile" | cut -d'=' -f2- | sort -u)
+		if [ -n "$possibleValue" ]; then
+			echo "$possibleValue"
+			rm -f "$tempBakedFile"
+			return 0
+		fi
+	else
+		returnState=$?
+		if [ "$returnState" -eq 1 ]; then
+			logWarning "No .env files found in ${dockerDir}."
+		else
+			logError "Failed to merge environment files"
+		fi
+		rm -f "$tempBakedFile"
+		return 2
+	fi
+
+	return 3
+}

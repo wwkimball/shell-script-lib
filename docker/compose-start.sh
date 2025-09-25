ab04464c4ea250310dacf0c7ac8afc5c911dcd85
William Kimball <30981667+wwkimball@users.noreply.github.com>
2025-09-25 11:39:49 -0500

    Add Apache-2.0 license

--- docker/compose-start.sh
+++ docker/compose-start.sh
@@ -34,8 +34,19 @@
 #   become responsive, to run a database migration process, or to run
 #   application-specific initialization scripts within the running container.
 #
-# Copyright 2025 William W. Kimball, Jr. MBA MSIS
-# All rights reserved.
+# Copyright 2021, 2024, 2025 William W. Kimball, Jr., MBA, MSIS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 ###############################################################################
 # Constants
 MY_VERSION='2025.04.18-1'
